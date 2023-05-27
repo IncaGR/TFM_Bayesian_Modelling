77e3ced9e6de7a036afa6fbd154c7e717913b859
Geraldo Gariza <45373415+IncaGR@users.noreply.github.com>
2023-05-27 21:02:13 +0100

    only lm

--- 2_code/2_R_code/3_data_modelling.R
+++ 2_code/2_R_code/3_data_modelling.R
@@ -126,6 +126,14 @@ lm_cook <- lm(reformulate(regressors,"log_price"),
 
 summary(lm_cook)
 
+
+
+date_to_save <- str_extract(path_modelling, "\\d{4}-\\d{2}-\\d{2}")
+
+path_to_save = paste0("C:/Users/ggari/Desktop/1_projects/TFM/1_data/2_data_Idealista/data_lm_cook_",date_to_save)
+
+saveRDS(data_cook,file=path_to_save)
+
 # # Extraño que con mas habitaciones menos precio, contra intuitivo.
 # betas <- as.data.frame(dfbetas(lm3))
 # 
@@ -234,128 +242,35 @@ summary(lm_cook)
 # install.packages("StanHeaders", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
 # install.packages("rstan", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
 
-library("rstan")
-library(bayesplot)
-# First we are going to try a simple bayesian model with covariate square_mt.
-
-options(mc.cores = parallel::detectCores())
-
-rstan_options(auto_write = TRUE)
-
-# price_pooled ------------------------------------------------------------
-
-N= nrow(data_cook)
-barri <- as.numeric(data_cook$id_barri)
-barri_name <- as.factor(unique(data_cook$barri))
-J <- length(unique(barri))
-y <- data_cook$log_price
-x <- log(data_cook$square_mt)
-# u <- radon %>% 
-#   group_by(county) %>% 
-#   summarise(u = first(log_uranium)) %>% 
-#   pull(u) # because is value per county 
-
-
-data_list <- list(
-  N = N,
-  y = y,
-  x = x
-)
-
-model_code <- "
-data {
-  int<lower=0> N;
-  vector[N] y;
-  vector<lower=0>[N] x;
-}
-parameters {
-  real a;
-  real b;                           
-  real<lower=0> sigma_y;
-}
-model {
-  y ~ normal(a + b * x, sigma_y);
-}
-"
-
-model = stan_model(model_code = model_code)
-
-# Fit the model to the data
-fit <- sampling(model, data = data_list, chains = 2, iter =2000)
-
-
-print(fit)
-plot(fit)
-
-
-
-price_summary <- tidy(fit, conf.int = T, level = 0.8, rhat = T, ess = T)
-
-df_pooled  <- tibble(
-  barri = barri_name,
-  model = "pooled",
-  intercept = price_summary$estimate[1],
-  slope = price_summary$estimate[2]
-)
-
+# library("rstan")
+# library(bayesplot)
+# # First we are going to try a simple bayesian model with covariate square_mt.
 # 
-# id_barrio<- c("la Guineueta",
-#                "la Vall d'Hebron", "Canyelles", "la Trinitat Nova", "el Raval", "la Dreta de l'Eixample", "el Barri Gòtic",
-#                "Sants")
-
-
-df_model <- df_pooled %>%
-  left_join(data_cook, by = "barri") 
-# %>%
-#   filter(barri %in% id_barrio)
-
-# ggplot(df_model) +
-#   geom_point(aes(log(square_mt), log_price)) +
-#   geom_abline(aes(intercept = intercept, slope = slope, color = model)) +
-#   facet_wrap(~ barri, ncol = 4) +
-#   theme(legend.position = "bottom")
-
-# Save document
-
-date_to_save <- str_extract(path_modelling, "\\d{4}-\\d{2}-\\d{2}")
-
-path_to_save = paste0("C:/Users/ggari/Desktop/1_projects/TFM/1_data/2_data_Idealista/data_modeled_pooled_",date_to_save)
-
-saveRDS(df_model,file=path_to_save)
-
-
-# price no pooled ---------------------------------------------------------
-
-data_list <- list(
-  N = N,
-  J = J,
-  y = y,
-  x = x,
-  barri = barri
-)
-
-# radon_2 <- stan("stan_models/radon_2_no_pooled_a.stan", iter = 1000, chains = 1,
-#                 data = data_list, seed = 1)
-
-model_code <- "
-data {
-  int<lower=0> N;
-  int<lower=0> J;
-  vector[N] y;
-  vector<lower=0>[N] x;
-  int barri[N];
-}
-parameters {
-  real a[J];
-  real b;              
-  real<lower=0> sigma_y;
-}
-model {
-  for (i in 1:N)
-    y[i] ~ normal(a[barri[i]] + b * x[i], sigma_y);
-}
-"
-
+# options(mc.cores = parallel::detectCores())
+# 
+# rstan_options(auto_write = TRUE)
+# 
+# # price_pooled ------------------------------------------------------------
+# 
+# N= nrow(data_cook)
+# barri <- as.numeric(data_cook$id_barri)
+# barri_name <- as.factor(unique(data_cook$barri))
+# J <- length(unique(barri))
+# y <- data_cook$log_price
+# x <- log(data_cook$square_mt)
+# # u <- radon %>% 
+# #   group_by(county) %>% 
+# #   summarise(u = first(log_uranium)) %>% 
+# #   pull(u) # because is value per county 
+# 
+# 
+# data_list <- list(
+#   N = N,
+#   y = y,
+#   x = x
+# )
+# 
+# model_code <- "
 # data {
 #   int<lower=0> N;
 #   vector[N] y;
@@ -369,34 +284,127 @@ model {
 # model {
 #   y ~ normal(a + b * x, sigma_y);
 # }
-
-model_2 = stan_model(model_code = model_code)
-
-# Fit the model to the data
-fit_2 <- sampling(model_2, data = data_list, chains = 1, iter =100)
-
-
-## Convergence analysis
-print(fit_2)
-plot(fit_2)
-
-price_summary <- tidy(fit_2, conf.int = T, level = 0.8, rhat = T, ess = T)
-
-df_no_pooled  <- tibble(
-  county = barri_name,
-  model = "no_pooled",
-  intercept = price_summary$estimate[1:J],
-  slope = price_summary$estimate[J+1]
-)
-
-df_model <- bind_rows(df_pooled, df_no_pooled) %>% 
-  left_join(data_cook, by = "barri") %>%
-  filter(barri %in% id_barrio) 
-
-ggplot(df_model) +
-  geom_jitter(aes(log(square_mt), log_price)) +
-  geom_abline(aes(intercept = intercept, slope = slope, color = model)) +
-  facet_wrap(~ barri, ncol = 4) + 
-  scale_x_continuous(breaks = 0:1) + 
-  theme(legend.position = "bottom")
-
+# "
+# 
+# model = stan_model(model_code = model_code)
+# 
+# # Fit the model to the data
+# fit <- sampling(model, data = data_list, chains = 2, iter =2000)
+# 
+# 
+# print(fit)
+# plot(fit)
+# 
+# 
+# 
+# price_summary <- tidy(fit, conf.int = T, level = 0.8, rhat = T, ess = T)
+# 
+# df_pooled  <- tibble(
+#   barri = barri_name,
+#   model = "pooled",
+#   intercept = price_summary$estimate[1],
+#   slope = price_summary$estimate[2]
+# )
+# 
+# # 
+# # id_barrio<- c("la Guineueta",
+# #                "la Vall d'Hebron", "Canyelles", "la Trinitat Nova", "el Raval", "la Dreta de l'Eixample", "el Barri Gòtic",
+# #                "Sants")
+# 
+# 
+# df_model <- df_pooled %>%
+#   left_join(data_cook, by = "barri") 
+# # %>%
+# #   filter(barri %in% id_barrio)
+# 
+# # ggplot(df_model) +
+# #   geom_point(aes(log(square_mt), log_price)) +
+# #   geom_abline(aes(intercept = intercept, slope = slope, color = model)) +
+# #   facet_wrap(~ barri, ncol = 4) +
+# #   theme(legend.position = "bottom")
+# 
+# # Save document
+# 
+# date_to_save <- str_extract(path_modelling, "\\d{4}-\\d{2}-\\d{2}")
+# 
+# path_to_save = paste0("C:/Users/ggari/Desktop/1_projects/TFM/1_data/2_data_Idealista/data_modeled_pooled_",date_to_save)
+# 
+# saveRDS(df_model,file=path_to_save)
+# 
+# 
+# # price no pooled ---------------------------------------------------------
+# 
+# data_list <- list(
+#   N = N,
+#   J = J,
+#   y = y,
+#   x = x,
+#   barri = barri
+# )
+# 
+# # radon_2 <- stan("stan_models/radon_2_no_pooled_a.stan", iter = 1000, chains = 1,
+# #                 data = data_list, seed = 1)
+# 
+# model_code <- "
+# data {
+#   int<lower=0> N;
+#   int<lower=0> J;
+#   vector[N] y;
+#   vector<lower=0>[N] x;
+#   int barri[N];
+# }
+# parameters {
+#   real a[J];
+#   real b;              
+#   real<lower=0> sigma_y;
+# }
+# model {
+#   for (i in 1:N)
+#     y[i] ~ normal(a[barri[i]] + b * x[i], sigma_y);
+# }
+# "
+# 
+# # data {
+# #   int<lower=0> N;
+# #   vector[N] y;
+# #   vector<lower=0>[N] x;
+# # }
+# # parameters {
+# #   real a;
+# #   real b;                           
+# #   real<lower=0> sigma_y;
+# # }
+# # model {
+# #   y ~ normal(a + b * x, sigma_y);
+# # }
+# 
+# model_2 = stan_model(model_code = model_code)
+# 
+# # Fit the model to the data
+# fit_2 <- sampling(model_2, data = data_list, chains = 1, iter =100)
+# 
+# 
+# ## Convergence analysis
+# print(fit_2)
+# plot(fit_2)
+# 
+# price_summary <- tidy(fit_2, conf.int = T, level = 0.8, rhat = T, ess = T)
+# 
+# df_no_pooled  <- tibble(
+#   county = barri_name,
+#   model = "no_pooled",
+#   intercept = price_summary$estimate[1:J],
+#   slope = price_summary$estimate[J+1]
+# )
+# 
+# df_model <- bind_rows(df_pooled, df_no_pooled) %>% 
+#   left_join(data_cook, by = "barri") %>%
+#   filter(barri %in% id_barrio) 
+# 
+# ggplot(df_model) +
+#   geom_jitter(aes(log(square_mt), log_price)) +
+#   geom_abline(aes(intercept = intercept, slope = slope, color = model)) +
+#   facet_wrap(~ barri, ncol = 4) + 
+#   scale_x_continuous(breaks = 0:1) + 
+#   theme(legend.position = "bottom")
+# 
