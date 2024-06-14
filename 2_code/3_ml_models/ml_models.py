ad57940d5d04999f9e1bd37af9af4481fdded849
Geraldo Gariza <45373415+IncaGR@users.noreply.github.com>
2024-06-14 09:37:50 +0200

    added more variables

--- 2_code/3_ml_models/ml_models.py
+++ 2_code/3_ml_models/ml_models.py
@@ -57,10 +57,73 @@ plt.yticks(())
 
 import xgboost as xgb
 
-xgb_m = xgb.XGBRegressor()
+# add max depth to avoid overfitting
+# learning rate or eta is the step size shrinkage used to prevent overfitting
+xgb_m = xgb.XGBRegressor(max_depth = 3, learning_rate = 0.3)
 
 xgb_m.fit(X_train, y_train)
 
 predictions = xgb_m.predict(X_test)
 
 print(r2_score(y_test, predictions))
+
+# add more variables to the models.
+
+# data = pd.DataFrame(np.arange(12).reshape((4,3)), columns=['a', 'b', 'c'])
+# # label = pd.DataFrame(np.random.randint(2, size=4))
+# dtrain = xgb.DMatrix(data)
+
+# # print(label)
+# print(data)
+# print(dtrain)
+
+# print(data.index)
+
+# print(data.loc[])
+
+data_c = data.copy()
+data_c = data_c[['price',
+                'rooms2',
+                'square_mt',
+                'wc2',
+                'terraza_balcon',
+                'amueblado',
+                'asc',
+                'calef',
+                'aire',
+                'exterior',
+                'casa',
+                'estudio',
+                'lujo',
+            #    'distrito2',
+            #    'regex_barris'
+                           ]]
+
+# data_c['distrito2'] = data_c['distrito2'].astype('category')
+# data_c['regex_barris'] = data_c['regex_barris'].astype('category')
+
+train, test = train_test_split(data_c, test_size=0.2, random_state=42)
+
+dtrain = xgb.DMatrix(train.loc[:,'rooms2':], label=train['price'], enable_categorical=True)
+dtest = xgb.DMatrix(test.loc[:,'rooms2':], label=test['price'], enable_categorical=True)
+
+xgb_m = xgb.XGBRegressor(max_depth = 3, learning_rate = 0.3)
+
+xgb_m.fit(dtrain.get_data(), dtrain.get_label())
+
+
+predictions = xgb_m.predict(dtest.get_data())
+
+print(r2_score(dtest.get_label(), predictions))
+
+# Plot outputs
+plt.scatter(train['square_mt'], train.price, color="black")
+plt.scatter(test['square_mt'], predictions, color="blue")
+
+plt.xticks(())
+plt.yticks(())
+
+# plt.show()
+
+# introducing K-fold cross validation
+
